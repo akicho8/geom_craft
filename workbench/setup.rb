67af25708efdfce0e1a87085c6c821a246e038ec
akicho8 <akicho8@gmail.com>
2025-04-14 21:16:41 +0900

    [feat] top_level_bind.rb → toplevel_binding.rb

--- workbench/setup.rb
+++ workbench/setup.rb
@@ -1,6 +1,5 @@
 $LOAD_PATH.unshift("../lib")
-require "geom_craft"
-require "geom_craft/top_level_bind"
+require "geom_craft/toplevel_binding"
 
 V                               # => GeomCraft::Vec2
 Range2                          # => GeomCraft::Range2
